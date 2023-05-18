d1189bca7d131ea4ac11abcc135ddc87c0772db5
Kvq5522 <89141917+Kvq5522@users.noreply.github.com>
2023-05-18 20:27:25 +0700

    fix: fix sign in notification

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -6,6 +6,10 @@ signInBtn.addEventListener('click', async() => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
+    let messageSection = document.getElementById('message');
+    messageSection.innerHTML = 'Đang xử lý...';
+    messageSection.style.color = 'blue';
+
     const data = await fetch('/access/signin', {
         method: 'POST',
         headers: {
@@ -22,7 +26,12 @@ signInBtn.addEventListener('click', async() => {
                 return window.location.href = '/dashboard';
             }
 
-            alert(data.message)
+            messageSection.innerHTML = "Email hoặc mật khẩu không đúng";
+            messageSection.style.color = 'red';
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+            messageSection.innerHTML = "Đã xảy ra lỗi";
+            messageSection.style.color = 'red';
+        });
 });
